96c51c2b503d2dcc4a942784c0841480035c3b8f
Luca Hodge <lucahodge@gmail.com>
2023-12-01 09:08:46 -0700

    removed some files

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/Users/lucahodge/src/advent-of-code/advent-of-code-2023/day01/target/debug/day01: /Users/lucahodge/src/advent-of-code/advent-of-code-2023/day01/src/main.rs
--- day01/target/debug/deps/day01-2b8e3249dc2dde26.d
+++
@@ -1,5 +0,0 @@
-/Users/lucahodge/src/advent-of-code/advent-of-code-2023/day01/target/debug/deps/day01-2b8e3249dc2dde26: src/main.rs
-
-/Users/lucahodge/src/advent-of-code/advent-of-code-2023/day01/target/debug/deps/day01-2b8e3249dc2dde26.d: src/main.rs
-
-src/main.rs:
