01fb4f6c2f246b19e7b4e5c0c4085b63f7a60491
Luca Hodge <lucahodge@gmail.com>
2023-12-04 13:46:10 -0700

    day 4 part 1

---
+++ day04/src/main.rs
@@ -0,0 +1,70 @@
+#[derive(Debug)]
+struct Card {
+    winning_numbers : Vec<i32>,
+    your_numbers : Vec<i32>,
+}
+
+fn parse_cards(file_name : &str) -> Vec<Card> {
+    let contents = std::fs::read_to_string(file_name).unwrap();
+    let lines : Vec<&str> = contents.lines().collect();
+    let mut cards = vec!();
+    for line in lines{
+        cards.push(parse_card(line));
+    }
+    return cards;
+}
+
+fn parse_card(line : &str) -> Card{
+    let lists = line.split(": ").collect::<Vec<&str>>()[1].split(" | ").collect::<Vec<&str>>();
+    Card{winning_numbers: parse_number_list(lists[0]), your_numbers: parse_number_list(lists[1])}
+}
+
+fn parse_number_list(list : &str) -> Vec<i32> {
+    let mut nl : Vec<&str> = list.split(" ").collect::<Vec<&str>>();
+    nl.retain(|x| {
+        match *x {
+            "" => false,
+            _ => true,
+        }
+    });
+    nl.into_iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+}
+
+fn number_of_matches (card: Card) -> i32 {
+    let mut matches = 0;
+    for your in card.your_numbers.iter(){
+        for winning in card.winning_numbers.iter(){
+            if your == winning {
+                matches += 1;
+            }
+        }
+    }
+    matches
+}
+
+fn part1(file_name : &str) -> i32 {
+    let cards = parse_cards(file_name);
+    let mut sum = 0;
+    for card in cards {
+        let num = number_of_matches(card);
+        sum += match num {
+            0 => 0,
+            n => 2_i32.pow((n - 1).try_into().unwrap()),
+        };
+    }
+    sum
+}
+
+fn main() {
+    println!("Part 1: {}", part1("src/cards.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part1_test() {
+        let result = part1("src/cards_test.txt");
+        assert_eq!(result, 13);
+    }
+}
