3c4e1ebe5387aa120f6319afc909418d16649e3c
Luca Hodge <lucahodge@gmail.com>
2023-12-06 13:53:40 -0700

    day 6 part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,8 +1,8 @@
 
 #[derive(Debug)]
 struct Race {
-    time : i32,
-    distance : i32,
+    time : i64,
+    distance : i64,
 }
 
 fn parse_races(file_name : &str) -> Vec<Race> {
@@ -10,21 +10,21 @@ fn parse_races(file_name : &str) -> Vec<Race> {
     let lines = contents.lines().collect::<Vec<&str>>();
     let time_line = lines[0];
     let distance_line = lines[1];
-    let times : Vec<i32> = time_line.split(" ").filter(|s|{
-        match s.parse::<i32>() {
+    let times : Vec<i64> = time_line.split(" ").filter(|s|{
+        match s.parse::<i64>() {
             Err(_) => false,
             Ok(_) => true,
         }
     }).map(|s| {
-        s.parse::<i32>().unwrap()
+        s.parse::<i64>().unwrap()
     }).collect();
-    let distances : Vec<i32> = distance_line.split(" ").filter(|s|{
-        match s.parse::<i32>() {
+    let distances : Vec<i64> = distance_line.split(" ").filter(|s|{
+        match s.parse::<i64>() {
             Err(_) => false,
             Ok(_) => true,
         }
     }).map(|s| {
-        s.parse::<i32>().unwrap()
+        s.parse::<i64>().unwrap()
     }).collect();
 
     let mut races = vec!();
@@ -35,7 +35,27 @@ fn parse_races(file_name : &str) -> Vec<Race> {
     races
 }
 
-fn number_of_ways_to_beat(race : Race) -> i32 {
+fn parse_race(file_name : &str) -> Race {
+    let contents = std::fs::read_to_string(file_name).unwrap();
+    let lines = contents.lines().collect::<Vec<&str>>();
+    let time_line = lines[0];
+    let distance_line = lines[1];
+    let time : i64 = time_line.chars().fold(0, |acc, curr| {
+        match curr.to_digit(10) {
+            Some(i) => acc * 10 + i as i64,
+            _ => acc,
+        }
+    });
+    let distance : i64 = distance_line.chars().fold(0, |acc, curr| {
+        match curr.to_digit(10) {
+            Some(i) => acc * 10 + i as i64,
+            _ => acc,
+        }
+    });
+    Race{time : time, distance : distance}
+}
+
+fn number_of_ways_to_beat(race : Race) -> i64 {
     let mut acc = 0;
     for i in 0..=race.time {
         let speed = i;
@@ -48,7 +68,7 @@ fn number_of_ways_to_beat(race : Race) -> i32 {
     acc
 }
 
-fn part1(races : Vec<Race>) -> i32 {
+fn part1(races : Vec<Race>) -> i64 {
     let mut acc = 1;
     for race in races {
         let num = number_of_ways_to_beat(race);
@@ -57,20 +77,33 @@ fn part1(races : Vec<Race>) -> i32 {
     acc
 }
 
+
+fn part2(race : Race) -> i64 {
+    number_of_ways_to_beat(race)
+}
+
 fn main() {
-    println!("Hello, world!");
     let races = parse_races("src/races.txt");
     let result1 = part1(races);
     println!("Part 1: {}", result1);
+    let race = parse_race("src/races.txt");
+    let result2 = part2(race);
+    println!("Part 2: {}", result2);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
+    fn part1_test() {
         let races = parse_races("src/races_test.txt");
         let result = part1(races);
         assert_eq!(result, 288);
+    }
+    #[test]
+    fn part2_test() {
+        let race = parse_race("src/races_test.txt");
+        let result = part2(race);
+        assert_eq!(result, 71503);
     }
 }
